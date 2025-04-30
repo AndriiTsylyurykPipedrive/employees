9ed8a4d3cea3052c31cbd91d7ede6b9b552afb28
AndriiTsylyurykPipedrive <andrii.tsylyuryk@pipedrive.com>
2025-04-30 10:54:02 +0300

    set up done

---
+++ employees-ui/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: "/",
+  plugins: [react()],
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  
+  server: {
+    strictPort: true,
+    host: true,
+    proxy: {
+      "/api": "http://localhost:3002",
+    },
+  },
+});
